997308d2e88ceaa9a55cda4b0601ccf15800be25
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2017-11-24 15:13:09 +0000

    init

---
+++ lib/OpusVL/Docker.pm
@@ -0,0 +1,26 @@
+package OpusVL::Docker;
+# ABSTRACT: Perl/docker utils by OpusVL
+our $VERSION = "0.001";
+1;
+
+=head1 DESCRIPTION
+
+This distribution contains both example and working files for various parts of a
+Docker/docker-compose setup. Files included are:
+
+=over
+
+=item C<bin/entrypoint>
+
+A working entrypoint for PSGI-based applications.
+
+=item C<Dockerfile.example.base>
+
+An example base Dockerfile from which to build your application.
+
+=item C<Dockerfile.example.patch>
+
+An example Dockerfile with which to patch the application you built with the
+base file.
+
+=back
